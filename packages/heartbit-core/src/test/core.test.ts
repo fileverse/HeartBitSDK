80db09850f821666d8371fa384b2aca9d8554c34
Joshua Onwuzu <joshuaonwuzu75@gmail.com>
2024-04-08 07:07:35 +0100

    refactored core test-cases

--- packages/heartbit-core/src/test/core.test.ts
+++ packages/heartbit-core/src/test/core.test.ts
@@ -1,7 +1,8 @@
-import { HeartBitCore } from './index';
-import { getMinterContract } from './utils';
+import { HeartBitCore } from '../index';
+import { getMinterContract } from '../utils';
 import { describe, expect, jest, it } from '@jest/globals';
-import { HEART_BIT_CONFIG } from './constants';
+import { HEART_BIT_CONFIG } from '../constants';
+import { mintData, unsignedMintData } from './mockData';
 
 jest.mock('./utils', () => ({
     getMinterContract: jest.fn().mockImplementation(() => {
@@ -13,12 +14,12 @@ jest.mock('./utils', () => ({
     }),
 }));
 
-  global.fetch = jest.fn(() =>
-    Promise.resolve(new Response(JSON.stringify({ success: true }), {
-      status: 200,
-      headers: { 'Content-Type': 'application/json' },
-    }))
-  );
+global.fetch = jest.fn(() =>
+Promise.resolve(new Response(JSON.stringify({ success: true }), {
+    status: 200,
+    headers: { 'Content-Type': 'application/json' },
+}))
+);
 
 describe('HeartBitCore', () => {
     const chain = '0xaa36a7';
@@ -35,13 +36,6 @@ describe('HeartBitCore', () => {
     });
 
     it('should call fetch with the correct parameters on mintHeartBit', async () => {
-        const mintData = {
-          message: "p6f7gr-5173.csb.app wants you to sign in with your Ethereum account:\n0x852bd84A4dcDc150648AE4e8D6D6e59b1797c86f\n\nHello World!\n\nURI: https://p6f7gr-5173.csb.app\nVersion: 1\nChain ID: undefined\nNonce: 6RnPMnIbRS1yQ02ZK\nIssued At: 2024-02-15T09:14:18.897Z",
-          signature: "0xfff4ce944d488abf2f8b2fd80bf3f585cbd14bbafe37ffd8b88e69ad88bd0c86426898416eb2c9c4f913eaa01430f4cac405c5a5c53a6410652b56874b9bd45e1c",
-          startTime: 1706898250,
-          endTime: 1706898251,
-          hash: "0xba0c379a9b364b41e69a6a00a8652e28cb7dda3ca684309b940807634919a940"
-        };
         await core.mintHeartBit(mintData);
 
         expect(fetch).toHaveBeenCalledWith(`${HEART_BIT_CONFIG[chain].relayerUrl}/signed-mint`, {
@@ -52,15 +46,7 @@ describe('HeartBitCore', () => {
     });
 
     it('should call fetch with the correct parameters on unSignedMintHeartBit', async () => {
-        const unsignedMintData = {
-          message: "p6f7gr-5173.csb.app wants you to sign in with your Ethereum account:\n0x852bd84A4dcDc150648AE4e8D6D6e59b1797c86f\n\nHello World!\n\nURI: https://p6f7gr-5173.csb.app\nVersion: 1\nChain ID: undefined\nNonce: 6RnPMnIbRS1yQ02ZK\nIssued At: 2024-02-15T09:14:18.897Z",
-          signature: "0xfff4ce944d488abf2f8b2fd80bf3f585cbd14bbafe37ffd8b88e69ad88bd0c86426898416eb2c9c4f913eaa01430f4cac405c5a5c53a6410652b56874b9bd45e1c",
-          startTime: 1706898250,
-          endTime: 1706898251,
-          hash: "0xba0c379a9b364b41e69a6a00a8652e28cb7dda3ca684309b940807634919a940",
-          account: "0x852bd84A4dcDc150648AE4e8D6D6e59b1797c86f",
-          apiKey: 'hello'
-        };
+
         const {apiKey, ...rest} = unsignedMintData
         await core.unSignedMintHeartBit(unsignedMintData);
 
---
+++ packages/heartbit-core/src/test/mockData.ts
@@ -0,0 +1,16 @@
+export const mintData = {
+    message: "p6f7gr-5173.csb.app wants you to sign in with your Ethereum account:\n0x852bd84A4dcDc150648AE4e8D6D6e59b1797c86f\n\nHello World!\n\nURI: https://p6f7gr-5173.csb.app\nVersion: 1\nChain ID: undefined\nNonce: 6RnPMnIbRS1yQ02ZK\nIssued At: 2024-02-15T09:14:18.897Z",
+    signature: "0xfff4ce944d488abf2f8b2fd80bf3f585cbd14bbafe37ffd8b88e69ad88bd0c86426898416eb2c9c4f913eaa01430f4cac405c5a5c53a6410652b56874b9bd45e1c",
+    startTime: 1706898250,
+    endTime: 1706898251,
+    hash: "0xba0c379a9b364b41e69a6a00a8652e28cb7dda3ca684309b940807634919a940"
+  };
+export const unsignedMintData = {
+    message: "p6f7gr-5173.csb.app wants you to sign in with your Ethereum account:\n0x852bd84A4dcDc150648AE4e8D6D6e59b1797c86f\n\nHello World!\n\nURI: https://p6f7gr-5173.csb.app\nVersion: 1\nChain ID: undefined\nNonce: 6RnPMnIbRS1yQ02ZK\nIssued At: 2024-02-15T09:14:18.897Z",
+    signature: "0xfff4ce944d488abf2f8b2fd80bf3f585cbd14bbafe37ffd8b88e69ad88bd0c86426898416eb2c9c4f913eaa01430f4cac405c5a5c53a6410652b56874b9bd45e1c",
+    startTime: 1706898250,
+    endTime: 1706898251,
+    hash: "0xba0c379a9b364b41e69a6a00a8652e28cb7dda3ca684309b940807634919a940",
+    account: "0x852bd84A4dcDc150648AE4e8D6D6e59b1797c86f",
+    apiKey: 'hello'
+  };
